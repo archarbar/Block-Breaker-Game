120948479a9a3c75c8511f241838e15a8c140998
Tony Ou <tony.ou@mail.mcgill.ca>
2019-03-01 13:44:51 -0500

    Added methods create/define game settings in the controller

--- BLOCK223/src/ca/mcgill/ecse223/block/controller/Block223Controller.java
+++ BLOCK223/src/ca/mcgill/ecse223/block/controller/Block223Controller.java
@@ -8,10 +8,86 @@ public class Block223Controller {
 	// Modifier methods
 	// ****************************
 	public static void createGame(String name) throws InvalidInputException {
+		UserRole currentUser = Block223Application.getCurrentUserRole();
+		if (!(currentUser instanceof Admin)) {
+			throw new InvalidInputException("Admin privileges are required to create a game");
+		}
+		Block223 block223 = Block223Application.getBlock223();
+		Admin admin = (Admin) currentUser;
+		try {
+			Game game = new Game(name, 1, admin, 1, 1, 1, 10, 10, block223);
+		}
+		catch (RuntimeException e) {
+			e.getMessage();
+		}
 	}
 
 	public static void setGameDetails(int nrLevels, int nrBlocksPerLevel, int minBallSpeedX, int minBallSpeedY,
 			Double ballSpeedIncreaseFactor, int maxPaddleLength, int minPaddleLength) throws InvalidInputException {
+		UserRole currentUser = Block223Application.getCurrentUserRole();
+		if (!(currentUser instanceof Admin)) {
+			throw new InvalidInputException("Admin privileges are required to define game settings");
+		}
+		Game currentGame = Block223Application.getCurrentGame();
+		if(currentGame == null) {
+			throw new InvalidInputException("A game must be selected to define game settings");
+		}
+		Admin admin = currentGame.getAdmin();
+		if(admin != (Admin) currentUser) {
+			throw new InvalidInputException("Only the admin who created the game can define its game settings");
+		}
+		if (nrLevels < 1 || nrLevels > 99) {
+			throw new InvalidInputException("The number of levels must be between 1 and 99");
+		}
+		try {
+			currentGame.setNrBlocksPerLevel(nrBlocksPerLevel);
+		}
+		catch (RuntimeException e) {
+			e.getMessage();
+		}
+		Ball ball = currentGame.getBall();
+		try {
+			ball.setMinBallSpeedX(minBallSpeedX);
+		}
+		catch (RuntimeException e) {
+			e.getMessage();
+		}
+		try {
+			ball.setMinBallSpeedY(minBallSpeedY);
+		}
+		catch (RuntimeException e) {
+			e.getMessage();
+		}
+		try {
+			ball.setBallSpeedIncreaseFactor(ballSpeedIncreaseFactor);
+		}
+		catch (RuntimeException e) {
+			e.getMessage();
+		}
+		Paddle paddle = currentGame.getPaddle();
+		try {
+			paddle.setMaxPaddleLength(maxPaddleLength);
+		}
+		catch (RuntimeException e) {
+			e.getMessage();
+		}
+		try {
+			paddle.setMinPaddleLength(minPaddleLength);
+		}
+		catch (RuntimeException e) {
+			e.getMessage();
+		}
+		List<Level> levels = currentGame.getLevels();
+		int size = levels.size();
+		while (nrLevels > size) {
+			currentGame.addLevel();
+			size = levels.size();
+		}
+		while (nrLevels < size) {
+			Level level = currentGame.getLevel(size-1);
+			level.delete();
+			size = levels.size();
+		}
 	}
 
 	public static void deleteGame(String name) throws InvalidInputException {
