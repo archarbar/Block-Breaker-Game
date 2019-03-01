f615b6da6e4ffe1d5e32724a15bd3976c0f5c359
Shi Tong Li <lishitongmy@gmail.com>
2019-03-01 13:22:19 -0500

    persistence layer done, model regenerated using persistence

--- BLOCK223/src/ca/mcgill/ecse223/block/model/Paddle.java
+++ BLOCK223/src/ca/mcgill/ecse223/block/model/Paddle.java
@@ -2,10 +2,12 @@
 /*This code was generated using the UMPLE 1.29.0.4181.a593105a9 modeling language!*/
 
 package ca.mcgill.ecse223.block.model;
+import java.io.Serializable;
 import java.util.*;
 
-// line 76 "../../../../../Block223 v2.ump"
-public class Paddle
+// line 68 "../../../../../Block223Persistence.ump"
+// line 78 "../../../../../Block223 v2.ump"
+public class Paddle implements Serializable
 {
 
   //------------------------
@@ -100,5 +102,13 @@ public class Paddle
             "maxPaddleLength" + ":" + getMaxPaddleLength()+ "," +
             "minPaddleLength" + ":" + getMinPaddleLength()+ "]" + System.getProperties().getProperty("line.separator") +
             "  " + "game = "+(getGame()!=null?Integer.toHexString(System.identityHashCode(getGame())):"null");
-  }
+  }  
+  //------------------------
+  // DEVELOPER CODE - PROVIDED AS-IS
+  //------------------------
+  
+  // line 71 "../../../../../Block223Persistence.ump"
+  private static final long serialVersionUID =007 ;
+
+  
 }
