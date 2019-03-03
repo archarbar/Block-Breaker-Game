f4b1e754539e21ca63e66847a8678cfe0e5f1f98
Tony Ou <tony.ou@mail.mcgill.ca>
2019-03-03 17:13:46 -0500

    Made changes to UI, again

--- BLOCK223/src/ca/mcgill/ecse223/block/view/Block223Page.java
+++ BLOCK223/src/ca/mcgill/ecse223/block/view/Block223Page.java
@@ -30,54 +30,19 @@ import ca.mcgill.ecse223.block.controller.TOGridCell;
 import ca.mcgill.ecse223.block.controller.TOUserMode;
 
 public class Block223Page extends JFrame {
-	
-	//UI elements
-	private JLabel errorMessage;
-	//Create game
-	private JTextField gameNameTextField;
-	private JLabel gameNameLabel;
-	private JButton createGameButton;
-	
+
 	//data elements
 	private String error = null;
-	
+
 	//Create new form Block223Page
 	public Block223Page() {
 		initComponents();
 		refreshData();
 	}
-	
+
 	private void initComponents() {
-		//elements for error message
-		errorMessage = new JLabel();
-		errorMessage.setForeground(Color.RED);
-		
-		//elements for new game
-		gameNameTextField = new JTextField();
-		gameNameLabel = new JLabel();
-		gameNameLabel.setText("Name:");
-		createGameButton = new JButton();
-		createGameButton.setText("Create Game");
-		
-		//global settings
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		setTitle("Block223 The Best Game Ever");
-		
-		//Listener for createGame button
-		createGameButton.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				createGameButtonActionPerformed(evt);
-			}
-		});
 	}
-	
+
 	private void refreshData() {
-		//error
-		errorMessage.setText(error);
-		if (error == null || error.length() == 0) {
-			//populate page with data
-			// game
-			gameNameTextField.setText("");
-		}
 	}
 }
