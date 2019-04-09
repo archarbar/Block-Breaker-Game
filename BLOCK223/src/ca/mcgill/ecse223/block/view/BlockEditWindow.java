803c60768f4954d657431a6498be30c1701877b5
Tony Ou <tony.ou@mail.mcgill.ca>
2019-04-09 19:43:53 -0400

    Fixed UI buttons

--- BLOCK223/src/ca/mcgill/ecse223/block/view/BlockEditWindow.java
+++ BLOCK223/src/ca/mcgill/ecse223/block/view/BlockEditWindow.java
@@ -2156,17 +2156,14 @@ public class BlockEditWindow extends JFrame {
 		lblDontForgetTo.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblDontForgetTo.setBounds(104, 431, 254, 40);
 		contentPane.add(lblDontForgetTo);
-		
-		JLabel lblevenAfterPressing = new JLabel("(EVEN AFTER PRESSING PUBLISH GAME)");
-		lblevenAfterPressing.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblevenAfterPressing.setBounds(104, 463, 254, 40);
-		contentPane.add(lblevenAfterPressing);
 	}
 	
 	public void publishGameActionPerformed(ActionEvent e) {
 		error = "";
 		try {
 			Block223Controller.publishGame();
+			Block223Controller.saveGame();
+			Block223Controller.logout();
 			RegisterLoginPage login = new RegisterLoginPage();
 			login.setVisible(true);
 			this.setVisible(false);
